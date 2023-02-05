7b6b722fc775288fc614bd99039ce89fa6630d22
LiliaSlabka <liaslabka@gmail.com>
2023-02-06 01:20:57 +0200

    02-video done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player')
+
+
+const player = new Player(iframe);
+
+videoTimer() 
+
+player.on('timeupdate', throttle(onPlay,1000))
+
+function onPlay (event) {
+    console.log(event.seconds)
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+}
+ 
+function videoTimer() {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    if (savedTime) {
+       player.setCurrentTime(savedTime)
+    }
+}
