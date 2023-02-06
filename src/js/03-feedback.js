18b47e0a7789ff5b563719cca229495c6b29ec96
LiliaSlabka <liaslabka@gmail.com>
2023-02-06 22:23:59 +0200

    03-feedback done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form')
+const textarea = document.querySelector('textarea')
+const input = document.querySelector('input')
+
+populateTextarea()
+
+form.addEventListener('submit', onFormSubmit)
+form.addEventListener('input', throttle(onTextareaInput, 500) )
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state')
+    console.log('Відправка форми')
+}
+
+function onTextareaInput() {
+    const data = {
+        email: input.value,
+        message: textarea.value
+    }
+    localStorage.setItem('feedback-form-state',JSON.stringify(data))
+}
+
+function populateTextarea() {
+    const savedData = localStorage.getItem('feedback-form-state')
+    if (savedData !== null) {
+        input.value = JSON.parse(savedData).email;
+        textarea.value = JSON.parse(savedData).message;
+    }
+    
+}
